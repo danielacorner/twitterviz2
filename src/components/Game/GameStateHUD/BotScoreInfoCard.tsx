3ab435fd8115f82557606fcb1fd0de0b9de42b6c
danielacorner <danielcorner7@gmail.com>
2021-09-05 22:38:38 -0400

    card styles

--- src/components/Game/GameStateHUD/BotScoreInfoCard.tsx
+++ src/components/Game/GameStateHUD/BotScoreInfoCard.tsx
@@ -34,6 +34,7 @@ const PADDING = 20;
 const WIDTH = 420;
 const HEIGHT = 480;
 const TRANSFORM = false;
+const AVATAR_WIDTH = 92;
 
 /** pops up from the bottom when we receive a bot score */
 export function BotScoreInfoCard() {
@@ -71,7 +72,7 @@ export function BotScoreInfoCard() {
 
   const [isDoneAnimating, setIsDoneAnimating] = useState(false);
   const springUpDown = useSpring({
-    transform: `translate3d(0,${isUp ? 0 : HEIGHT * 0.75}px,0)`,
+    transform: `translate3d(0,${isUp ? -HEIGHT / 2 : HEIGHT}px,0)`,
     opacity: isUp ? 1 : 0,
     position: "fixed" as any,
     zIndex: 9999999999999999,
@@ -82,11 +83,10 @@ export function BotScoreInfoCard() {
     },
   });
 
-  const AVATAR_WIDTH = 92;
   const springExpandOnMount = useSpring({
-    width: `calc(${
+    width: `${
       isUp && latestNodeWithBotScore && isDoneAnimating ? scorePercent : 0
-    }% - 12px)`,
+    }%`,
     immediate: !isUp,
     config: config.molasses,
   });
@@ -114,39 +114,33 @@ export function BotScoreInfoCard() {
       <animated.div style={springUpDown}>
         <HtmlBotScoreInfoOverlayStyles {...{ botTypeText, scorePercent, isUp }}>
           <div className="content">
-            <AvatarStyles
-              customCss={`
-              z-index: 9999999999999;
-              position: absolute;
-              top: -${AVATAR_WIDTH / 2}px;
-              left: calc(1.2em + 4px);
-              height: ${AVATAR_WIDTH}px;
-              width: ${AVATAR_WIDTH}px;
-              img{width:100%;height:100%;}
-              box-shadow: 0px 2px 3px rgba(0,0,0,0.5);
-              border: 4px solid ${darkBackground};
-          `}
-            >
-              <img
-                src={latestNodeWithBotScore?.user?.profile_image_url_https}
-                alt=""
-              />
-            </AvatarStyles>
-            <div className="scrollContent">
-              <div className="displayName">{lastNode?.user.name}</div>
-              <div className="screenName">{lastNode?.user.screen_name}</div>
-              <div className="botType">
-                <animated.div
-                  style={{
-                    ...springExpandOnMount,
-                  }}
-                  className="percentWidth"
-                ></animated.div>
-                <div className="percentWidthOutline"></div>
-                {botType}
-                <span className="scorePercent"> — {scorePercent}%</span>
+            <div className="topRow">
+              <div className="avatarWrapper col1">
+                <AvatarStyles
+                  customCss={`
+                z-index: 9999999999999;
+                height: ${AVATAR_WIDTH}px;
+                width: ${AVATAR_WIDTH}px;
+                img{width:100%;height:100%;}
+                box-shadow: 0px 2px 3px rgba(0,0,0,0.5);
+                border: 4px solid ${darkBackground};
+            `}
+                >
+                  <img
+                    src={latestNodeWithBotScore?.user?.profile_image_url_https}
+                    alt=""
+                  />
+                </AvatarStyles>
               </div>
-              {botTypeInfo && <div className="botTypeInfo">{botTypeInfo}</div>}
+
+              <div className="col2">
+                <div className="screenName cardTitle">
+                  {lastNode?.user.screen_name}
+                </div>
+                <div className="displayName">{lastNode?.user.name}</div>
+              </div>
+            </div>
+            <div className="scrollContent">
               <div className="botScoreLegendCanvas">
                 <Suspense fallback={null}>
                   <Canvas style={{ overflow: "visible" }}>
@@ -165,7 +159,18 @@ export function BotScoreInfoCard() {
                   </Canvas>
                 </Suspense>
               </div>
-
+              <div className="botType">
+                <animated.div
+                  style={{
+                    ...springExpandOnMount,
+                  }}
+                  className="percentWidth"
+                ></animated.div>
+                <div className="percentWidthOutline"></div>
+                {botType}
+                <span className="scorePercent"> — {scorePercent}%</span>
+              </div>
+              {botTypeInfo && <div className="botTypeInfo">{botTypeInfo}</div>}
               <div className="botScoreInfo">
                 <span className="screenName" style={{ marginRight: 4 }}>
                   {latestNodeWithBotScore?.user.screen_name}
@@ -186,6 +191,9 @@ export function BotScoreInfoCard() {
   );
 }
 
+const BOTTYPE_PADDING_INNER = 10;
+const CARD_BORDER_WIDTH = 12;
+
 const HtmlBotScoreInfoOverlayStyles = styled.div`
   font-family: "Roboto", sans-serif;
   transition: transform 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
@@ -198,17 +206,44 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
     : "none"}; */
   box-sizing: border-box;
   font-size: ${TRANSFORM ? 4 : 24}px;
+  .topRow {
+    display: flex;
+    gap: 16px;
+    height: 64px;
+    margin-top: ${CARD_BORDER_WIDTH / 2}px;
+    margin-left: ${-CARD_BORDER_WIDTH / 2}px;
+  }
+  .col1,
+  .col2 {
+    display: flex;
+    flex-direction: column;
+  }
+  .col2 {
+    justify-content: center;
+  }
   .content {
     pointer-events: ${(p) => (p.isUp ? "auto" : "none")};
+    border: ${CARD_BORDER_WIDTH}px solid #000;
+
+    .avatarWrapper {
+      height: ${AVATAR_WIDTH}px;
+
+      width: ${AVATAR_WIDTH}px;
+      margin-top: ${-CARD_BORDER_WIDTH}px;
+    }
     .scrollContent {
       width: 100%;
       height: fit-content;
       max-height: calc(100vh - 96px);
       ${CUSTOM_SCROLLBAR_CSS}
-      padding: 0.5em 1em 1rem;
+      padding: 0 1em 0;
     }
     .botScoreLegendCanvas {
       height: 320px;
+      padding-top: 6px;
+      margin-bottom: 6px;
+      border-right: 3px solid #555555;
+      border-left: 3px solid #555555;
     }
     box-shadow: 0px 1px 11px #0000009c;
     border-radius: 16px;
@@ -226,8 +261,11 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
   }
   .btnClose {
     position: absolute;
-    top: 0px;
-    right: 0px;
+    position: absolute;
+    top: -19px;
+    right: -21px;
+    z-index: 2;
+    opacity: 0.9;
     /* background: #ffffff3d; */
   }
   .MuiSvgIcon-root {
@@ -235,30 +273,58 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
   }
   .botScoreInfo {
     margin-top: 1em;
+    padding-bottom: 1.5em;
   }
   .botTypeInfo {
     font-style: italic;
     line-height: 1.6em;
     font-size: 16px;
     color: ${colorSecondary};
+    margin-top: 0.5em;
     margin-bottom: 1em;
-  }
-  .displayName {
-    font-size: 1.2em;
-    margin: 1.2em 0 0.2em;
-    text-align: left;
   }
   .screenName {
     color: ${textSecondaryColor};
     font-size: 1em;
-    margin: 0em 1em 0.5em;
     text-align: left;
     position: relative;
     width: fit-content;
+    text-shadow: 2px 2px 1px black;
+    padding-left: 1em;
+    z-index: 1;
     &:before {
       position: absolute;
-      left: -1em;
+      left: 0;
       content: "@";
+    }
+    &.cardTitle:after {
+      position: absolute;
+      left: -52px;
+      right: -128px;
+      border-top-right-radius: 52px;
+      bottom: 0;
+      top: 0;
+      background: #0b1015;
+      z-index: -1;
+      content: "";
+    }
+  }
+  .displayName {
+    font-size: 1em;
+    width: fit-content;
+    text-align: left;
+    position: relative;
+    z-index: 1;
+    &:after {
+      position: absolute;
+      left: -52px;
+      right: -230px;
+      border-bottom-right-radius: 26px;
+      bottom: 0;
+      top: 0;
+      background: #2a4553;
+      z-index: -1;
+      content: "";
     }
   }
   .screenName,
@@ -267,7 +333,7 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
   }
   .botType {
     position: relative;
-    left: 6px;
+    left: 0px;
     .percentWidth {
       position: absolute;
       height: 40px;
@@ -276,16 +342,16 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
       border-radius: 6px;
       top: -6px;
       bottom: -6px;
-      left: -6px;
+      left: -${BOTTYPE_PADDING_INNER}px;
+      right: -${BOTTYPE_PADDING_INNER}px;
     }
     .percentWidthOutline {
       border-radius: 6px;
       position: absolute;
       height: 32px;
-      width: 100%;
       padding: 20px;
-      left: -6px;
-      right: -6px;
+      left: -${BOTTYPE_PADDING_INNER}px;
+      right: -${BOTTYPE_PADDING_INNER}px;
       top: -6px;
       border: 1px solid #ffffff30;
       margin: -0.5px;
@@ -309,7 +375,7 @@ const HtmlBotScoreInfoOverlayStyles = styled.div`
       box-sizing: content-box;
       .scrollContent {
         box-sizing: border-box;
-        padding: 0.5em 1.2em 2em;
+        padding: 0 1.2em 0;
       }
     }
   }
